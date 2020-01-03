0096adb1f1aa55f85a264131bbe79aef2b9af4fd
Dal Yoon <dalyoon@cpe-172-100-101-145.twcny.res.rr.com>
2020-01-03 15:58:42 +0900

    modify image

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,27 @@
 import "package:flutter/material.dart";
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("EasyList"),
-        ),
-        body: Card(
-          child: Column(
-            children: <Widget>[
-              Image.asset("assets/nice.png"),
-              Text("Food Paradise")
-            ],
+          appBar: AppBar(
+            title: Text("EasyList"),
           ),
-        ),
-      ),
+          body: Column(
+            children: <Widget>[
+              Card(
+                child: Column(
+                  children: <Widget>[
+                    Image.asset("assets/jake1.png"),
+                    Text("Adventure Time")
+                  ],
+                ),
+              ),
+            ],
+          )),
     );
   }
 }
